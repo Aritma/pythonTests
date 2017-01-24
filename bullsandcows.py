a3f2497224d7b50612a9274824994a7a3ca4606e
Aritma <aritma.cz@gmail.com>
2017-01-24 11:14:27 +0100

    Add files via upload

---
+++ bullsandcows.py
@@ -0,0 +1,53 @@
+import random
+
+def generateRandUnique(digits):
+    number=random.sample(range(0,9),digits)
+    while number[0]==0:
+        number=random.sample(range(0,9),digits)
+
+    print("Random number generated.")
+    print("Random number contains " + str(digits) + " digits. Never starts with 0.")
+    return number
+
+def isUniqueDigits(number,digits):
+    if number.isdigit() and len(number)==digits and len(set(number))==digits:
+        return True
+    else:
+        return False
+
+def getInput(randomNumber):
+    val=input("Guess value: ")
+    while not isUniqueDigits(val,len(randomNumber)):
+        print("Incorrect input. Try again...")
+        val=input("Guess value: ")
+    valList=[int(i) for i in list(val)]
+    return valList
+    
+
+def getLivestock(num1list, num2list):
+    cows=0
+    bulls=0
+    for i in num2list:
+        if i in num1list:
+            cows+=1
+            if num1list.index(i)==num2list.index(i):
+                cows-=1
+                bulls+=1
+    print("COWS: " + str(cows))
+    print("BULLS: " + str(bulls))
+    return [cows,bulls]
+
+def main():
+    randomNumber=generateRandUnique(4)
+    guessCounter=0
+
+    while True:
+        testList=getInput(randomNumber)
+        guessCounter+=1
+        #print("DEBUG VALUE: RANDOM NUMBER LIST = " + str(randomNumber))
+        if getLivestock(randomNumber,testList)[1]==len(randomNumber):
+            print("You win! (Total guesses: " + str(guessCounter) + ")")
+            break
+
+if __name__ == "__main__":
+    main()
